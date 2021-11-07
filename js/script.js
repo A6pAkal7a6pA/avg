5e471cc41925469797aa34ee8c48391f6abd9069
Maksym Kuleshov <maksym_kuleshov@epam.com>
2021-11-07 23:56:14 +0200

    make avg site without responsive

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// new Vivus('my-svg', {
+// 	type: 'oneByOne',
+// 	duration: 100,
+// 	animTimingFunction: Vivus.EASE_IN
+// }, function (myVivus) {
+// 	setTimeout(function () {
+// 		myVivus.play(myVivus.getStatus() === 'end' ? -1 : 1);
+// 	}, 0);
+// })
+
+// new Vivus('planet', {
+// 	type: 'oneByOne',
+// 	duration: 100,
+// 	animTimingFunction: Vivus.EASE
+// }, function (myVivus) {
+// 	setTimeout(function () {
+// 		myVivus.play(myVivus.getStatus() === 'end' ? -1 : 1);
+// 	}, 100);
+// })
+
+let modeButton = document.querySelector('#toggle');
+let mode = document.querySelector('.wrapper');
+
+modeButton.addEventListener('click', (e) => {
+	if (modeButton.checked === false) {
+		mode.classList.add('dark');
+		mode.classList.remove('light');
+	} else {
+		mode.classList.add('light');
+		mode.classList.remove('dark');
+	}
+});
+$(document).ready(function(){
+	$(".menu").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+					top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
